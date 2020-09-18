912136e1c489fa8d495a91f744dd7713239d7b48
sohaib shaikh <shaikhsohaib84@gmail.com>
2020-09-18 07:18:15 +0530

    path of dist/blogdemo set in server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/blogdemo'));
+// Serve only the static files form the dist directory 
+app.use(express.static(__dirname + 'dist/blogdemo'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/blogdemo/index.html'));
+res.sendFile(path.join(__dirname+'dist/blogdemo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
